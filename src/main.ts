f678e65bbcebf6e74e7c315769736b01aec40179
Abid Hasan Muin <muin.739@gmail.com>
2025-03-12 14:27:09 +0600

    added tracer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import tracer from './tracer';
 
 async function bootstrap() {
+  tracer.start();
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
